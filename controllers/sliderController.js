0d93b7a5c9bc252eb14f789500400aae7b50bb94
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-22 18:57:33 +0400

    configure-slider-contoller

---
+++ controllers/sliderController.js
@@ -0,0 +1,22 @@
+const Slider = require('../models/Slider')
+const {StatusCodes} = require('http-status-codes')
+
+
+const createSlider = async (req,res) =>{
+
+}
+
+const getSliders = async (req,res) =>{
+    
+}
+
+const updateSlider = async (req,res) =>{
+    
+}
+
+const deleteSlider = async (req,res) =>{
+    
+}
+
+
+module.exports = {createSlider,updateSlider,getSliders,deleteSlider}
