65fc3d160899e70706da65a3429a9531789f4c98
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-02-18 15:52:37 +0400

    --

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const login = async (req, res) => {
         }
         refreshToken = existingToken.refreshToken
         attachCookiesToResponse({ res, user, refreshToken })
-        res.status(StatusCodes.OK).json({ user: tokenUser , basketItems : user.basketItems })
+        res.status(StatusCodes.OK).json({ user: tokenUser , basketItems : user.basketItems , orders:user.orders })
         return;
     }
 
@@ -49,7 +49,8 @@ const login = async (req, res) => {
 
     attachCookiesToResponse({ res, user, refreshToken })
 
-    res.status(StatusCodes.OK).json({ user: tokenUser , basketItems : user.basketItems })
+    res.status(StatusCodes.OK).json({ user: tokenUser , basketItems : user.basketItems , orders:user.orders })
+
 }
 
 
